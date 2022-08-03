2eb4d91f7bbedd12285779becd603e58b8e45ed0
rezaulkhan.gakk <rezaulkhan.gakk@gmail.com>
2022-08-03 13:43:40 +0600

    add adapter..

--- ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/adapter/MusicAdapter.kt
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/adapter/MusicAdapter.kt
@@ -1,4 +1,25 @@
 package com.shadhinmusiclibrary.adapter
 
-class MusicAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shadhinmusiclibrary.adapter.view_holder.BaseViewHolder
+
+internal class MusicAdapter() : RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MusicViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MusicViewHolder(itemView: View) : BaseViewHolder(itemView) {
+
+    }
 }
