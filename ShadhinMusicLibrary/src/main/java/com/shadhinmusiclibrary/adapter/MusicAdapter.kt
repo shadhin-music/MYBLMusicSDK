982fa0489bc57cfb80d815b27c40eaaf41a82a25
rezaulkhan.gakk <rezaulkhan.gakk@gmail.com>
2022-08-03 15:03:00 +0600

    add view item

--- ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/adapter/MusicAdapter.kt
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/adapter/MusicAdapter.kt
@@ -1,22 +1,27 @@
 package com.shadhinmusiclibrary.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.shadhinmusiclibrary.R
 import com.shadhinmusiclibrary.adapter.view_holder.BaseViewHolder
 
 internal class MusicAdapter() : RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
-        TODO("Not yet implemented")
+        return MusicViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.music_view_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: MusicViewHolder, position: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 0;
     }
 
     inner class MusicViewHolder(itemView: View) : BaseViewHolder(itemView) {
