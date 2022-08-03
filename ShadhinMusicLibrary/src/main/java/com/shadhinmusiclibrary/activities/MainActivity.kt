0c7880fca9b629e0860335f085a0146c757160c6
Mehnaz <mehnaz.gakk@gmail.com>
2022-08-03 12:23:22 +0600

    first commit

---
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/activities/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.shadhinmusiclibrary.activities
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import com.shadhinmusiclibrary.R
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(binding.root)
+
+
+
+
+
+
+    }
+
+
+}
