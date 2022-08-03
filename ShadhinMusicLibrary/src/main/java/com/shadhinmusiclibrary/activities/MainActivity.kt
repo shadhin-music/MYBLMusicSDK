60b78f90ecd236e10b47cda41b442d2b5b557914
Mehnaz <mehnaz.gakk@gmail.com>
2022-08-03 12:52:14 +0600

    tab added commit

--- ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/activities/MainActivity.kt
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/activities/MainActivity.kt
@@ -15,8 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        setContentView(binding.root)
+        
 
 
 
