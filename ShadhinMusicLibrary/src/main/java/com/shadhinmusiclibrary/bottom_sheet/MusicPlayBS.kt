5bd696cb04757f147d0beb2402bab08d51c3b6e0
rezaulkhan.gakk <rezaulkhan.gakk@gmail.com>
2022-09-11 12:21:09 +0600

    music player ui funsonality running

--- ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/bottom_sheet/MusicPlayBS.kt
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/bottom_sheet/MusicPlayBS.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import com.shadhinmusiclibrary.R
 import com.shadhinmusiclibrary.adapter.MusicPlayAdapter
@@ -13,13 +18,62 @@ import com.shadhinmusiclibrary.library.discretescrollview.DiscreteScrollView
 
 class MusicPlayBS : Fragment() {
 
+    private lateinit var acivMinimizePlayerBtn: AppCompatImageView
+    private lateinit var tvTitle: AppCompatImageView
+    private lateinit var acivMenu: AppCompatImageView
+    private lateinit var dsvCurrentPlaySongsThumb: DiscreteScrollView
+    private lateinit var tvSongName: TextView
+    private lateinit var tvSingerName: TextView
+    private lateinit var ivFavoriteBtn: ImageView
+    private lateinit var sbCurrentPlaySongStatus: SeekBar
+    private lateinit var tvCurrentPlayDuration: TextView
+    private lateinit var tvTotalPlayDuration: TextView
+    private lateinit var ibtnShuffle: ImageButton
+    private lateinit var ibtnSkipPrevious: ImageButton
+    private lateinit var ibtnPlayPause: ImageButton
+    private lateinit var ibtnSkipNext: ImageButton
+    private lateinit var ibtnRepeatSong: ImageButton
+    private lateinit var ibtnVolume: ImageButton
+    private lateinit var ibtnLibraryAdd: ImageButton
+    private lateinit var ibtnQueueMusic: ImageButton
+    private lateinit var ibtnDownload: ImageButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.main_music_player2, container, false)
-        val dsvCurrentPlaySongsThumb: DiscreteScrollView =
-            view.findViewById(R.id.dsv_current_play_songs_thumb)
+        initUI(view)
+
+        setMusicBannerAdapter()
+        uiFunctionality()
+
+        return view
+    }
+
+    private fun initUI(viewRef: View) {
+        acivMinimizePlayerBtn = viewRef.findViewById(R.id.aciv_minimize_player_btn)
+        tvTitle = viewRef.findViewById(R.id.tvTitle)
+        acivMenu = viewRef.findViewById(R.id.aciv_menu)
+        dsvCurrentPlaySongsThumb = viewRef.findViewById(R.id.dsv_current_play_songs_thumb)
+        tvSongName = viewRef.findViewById(R.id.tv_song_name)
+        tvSingerName = viewRef.findViewById(R.id.tv_singer_name)
+        ivFavoriteBtn = viewRef.findViewById(R.id.iv_favorite_btn)
+        sbCurrentPlaySongStatus = viewRef.findViewById(R.id.sb_current_play_song_status)
+        tvCurrentPlayDuration = viewRef.findViewById(R.id.tv_current_play_duration)
+        tvTotalPlayDuration = viewRef.findViewById(R.id.tv_total_play_duration)
+        ibtnShuffle = viewRef.findViewById(R.id.ibtn_shuffle)
+        ibtnSkipPrevious = viewRef.findViewById(R.id.ibtn_skip_previous)
+        ibtnPlayPause = viewRef.findViewById(R.id.ibtn_play_pause)
+        ibtnSkipNext = viewRef.findViewById(R.id.ibtn_skip_next)
+        ibtnRepeatSong = viewRef.findViewById(R.id.ibtn_repeat_song)
+        ibtnVolume = viewRef.findViewById(R.id.ibtn_volume)
+        ibtnLibraryAdd = viewRef.findViewById(R.id.ibtn_library_add)
+        ibtnQueueMusic = viewRef.findViewById(R.id.ibtn_queue_music)
+        ibtnDownload = viewRef.findViewById(R.id.ibtn_download)
+    }
+
+    private fun setMusicBannerAdapter() {
         val adapter = MusicPlayAdapter()
         val listData = mutableListOf<HomePatchDetail>()
         for (i in 1..3) {
@@ -65,6 +119,25 @@ class MusicPlayBS : Fragment() {
         dsvCurrentPlaySongsThumb.setOffscreenItems(2)
         dsvCurrentPlaySongsThumb.setItemTransitionTimeMillis(150)
         dsvCurrentPlaySongsThumb.itemAnimator = null
-        return view
+    }
+
+    private fun uiFunctionality() {
+        ibtnShuffle.setOnClickListener { }
+
+        ibtnSkipPrevious.setOnClickListener { }
+
+        ibtnPlayPause.setOnClickListener { }
+
+        ibtnSkipNext.setOnClickListener { }
+
+        ibtnRepeatSong.setOnClickListener { }
+
+        ibtnVolume.setOnClickListener { }
+
+        ibtnLibraryAdd.setOnClickListener { }
+
+        ibtnQueueMusic.setOnClickListener { }
+
+        ibtnDownload.setOnClickListener { }
     }
 }
