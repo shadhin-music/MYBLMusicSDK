53552de78faf0750345b3aaafb5ee04d8ecd7746
Mehnaz <mehnaz.gakk@gmail.com>
2022-09-25 18:02:56 +0600

    album details added

--- app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
+++ app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.shadhinmusiclibrary.ShadhinMusicSdkCore
 
 
 class AppActivity() : AppCompatActivity() {
@@ -19,10 +20,10 @@ class AppActivity() : AppCompatActivity() {
 //        val buttonHome:Button = findViewById(R.id.buttonHome)
 //        val buttonAPI: Button = findViewById(R.id.buttonAPI)
 //        buttonHome.setOnClickListener {
-////                ShadhinMusicSdkCore.getHomeFragment()
-//            val transaction = supportFragmentManager.beginTransaction()
-//        transaction.replace(R.id.app_home_fragment, ShadhinMusicSdkCore.getHomeFragment())
-//        transaction.commit()
+//                ShadhinMusicSdkCore.getHomeFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.app_home_fragment, ShadhinMusicSdkCore.getHomeFragment())
+        transaction.commit()
 //        }
 
 
@@ -31,36 +32,36 @@ class AppActivity() : AppCompatActivity() {
 //            finish()
 //        }, 1000)
 
-        // HomeFragment()
-        tabLayout = findViewById(R.id.tabLayout)
-        viewPager = findViewById(R.id.viewPager)
-
-        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
-        val adapter = ViewPagerAdapter(
-            this, supportFragmentManager,
-            tabLayout.tabCount
-        )
-        viewPager.adapter = adapter
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {
-            }
-        })
-
-
-//       scope?.launch(Dispatchers.Main) {
-        viewPager.offscreenPageLimit = 2
-        val selectedTabIndex = 0
-        viewPager.setCurrentItem(selectedTabIndex, false)
+//        // HomeFragment()
+//        tabLayout = findViewById(R.id.tabLayout)
+//        viewPager = findViewById(R.id.viewPager)
+//
+//        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+//        val adapter = ViewPagerAdapter(
+//            this, supportFragmentManager,
+//            tabLayout.tabCount
+//        )
+//        viewPager.adapter = adapter
+//        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//
+//            override fun onTabSelected(tab: TabLayout.Tab) {
+//                viewPager.currentItem = tab.position
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab) {
+//
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab) {
+//            }
+//        })
+//
+//
+////       scope?.launch(Dispatchers.Main) {
+//        viewPager.offscreenPageLimit = 2
+//        val selectedTabIndex = 0
+//        viewPager.setCurrentItem(selectedTabIndex, false)
     }
 
 
