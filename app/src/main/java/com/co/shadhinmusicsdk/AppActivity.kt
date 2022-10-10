04855acaea97634feb2163fc72c0a37692442775
“rezaulkhan111” <rezaulkhan111@hotmail.com>
2022-10-10 12:47:57 +0600

    project downgrade and new development code. from rezaul

--- app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
+++ app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.shadhinmusiclibrary.ShadhinMusicSdkCore
 
 
 class AppActivity() : AppCompatActivity() {
@@ -34,10 +33,12 @@ class AppActivity() : AppCompatActivity() {
         viewPager = findViewById(R.id.viewPager)
 
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
         val adapter = ViewPagerAdapter(
             this, supportFragmentManager,
             tabLayout.tabCount
         )
+
         viewPager.adapter = adapter
         viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
