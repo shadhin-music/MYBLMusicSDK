5b2bc444c94f241a5e22fde3ffa54911789588c1
Mehnaz <mehnaz.gakk@gmail.com>
2022-10-11 10:36:31 +0600

    Merged with tonmoy and reza vai's code

--- app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
+++ app/src/main/java/com/co/shadhinmusicsdk/AppActivity.kt
@@ -30,34 +30,34 @@ class AppActivity() : AppCompatActivity() {
 //        }, 1000)
 
         // HomeFragment()
-//        tabLayout = findViewById(R.id.tabLayout)
-//        viewPager = findViewById(R.id.viewPager)
-//
-//        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
-//
-//        val adapter = ViewPagerAdapter(
-//            this, supportFragmentManager,
-//            tabLayout.tabCount
-//        )
-//
-//        viewPager.adapter = adapter
-//        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-//        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//
-//            override fun onTabSelected(tab: TabLayout.Tab) {
-//                viewPager.currentItem = tab.position
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab) {
-//
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab) {
-//            }
-//        })
-////       scope?.launch(Dispatchers.Main) {
-//        viewPager.offscreenPageLimit = 2
-//        val selectedTabIndex = 0
-//        viewPager.setCurrentItem(selectedTabIndex, false)
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
+
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = ViewPagerAdapter(
+            this, supportFragmentManager,
+            tabLayout.tabCount
+        )
+
+        viewPager.adapter = adapter
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        })
+//       scope?.launch(Dispatchers.Main) {
+        viewPager.offscreenPageLimit = 2
+        val selectedTabIndex = 0
+        viewPager.setCurrentItem(selectedTabIndex, false)
     }
 }
