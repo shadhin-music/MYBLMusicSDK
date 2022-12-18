9c5456ee1fbfac585f47938fbd66f06615759cf3
Mehnaz <mehnaz.gakk@gmail.com>
2022-12-18 16:12:47 +0600

    fix

--- ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/fragments/album/AlbumDetailsFragment.kt
+++ ShadhinMusicLibrary/src/main/java/com/shadhinmusiclibrary/fragments/album/AlbumDetailsFragment.kt
@@ -157,7 +157,7 @@ internal class AlbumDetailsFragment : BaseFragment(),
                 if (res.status == Status.SUCCESS) {
                     progressBar.visibility = GONE
                     if (res.data?.data != null && argHomePatchDetail != null) {
-                        homeDetails.titleName = res.data.name
+                   
                         albumsTrackAdapter.setData(
                             res.data.data,
                             homeDetails,
